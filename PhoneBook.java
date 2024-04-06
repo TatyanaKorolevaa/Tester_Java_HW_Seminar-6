81130dff43e6fae7e05d3d254be9c74339a8be0d
TatyanaKorolevaa <[200~t310kor233@mail.ru~>
2024-04-06 22:01:15 +0300

    Program

--- PhoneBook.java
+++ PhoneBook.java
@@ -19,21 +19,11 @@ public class PhoneBook {
         if (!isExists[0]) {
             phoneBook.put(++this._ai, new PhoneInfo(unitName, phoneNum));
         }
-        this.wasAdded(this._ai);
     }
 
     public static HashMap<Integer, PhoneInfo> getPhoneBook() {
         return phoneBook;
     }
 
-    public void wasAdded(int idx) {
-        String str = new String(
-            String.format("У контакта %s с id(%d) номер телефона %d", 
-                phoneBook.get(idx).getName(), 
-                idx, 
-                phoneBook.get(idx).getPhoneNumberCounter()
-            )
-        );
-        System.out.println(str);
-    }
+
 }
--- Program.java
+++ Program.java
@@ -7,28 +7,38 @@ public class Programm {
         String name2;
         int phone1;
         int phone2;
+        int phone3;
+        int phone4;
+        int phone5;
         
 
         if (args.length == 0) {
             name1 = "Митрофанова Ирина";
             name2 = "Васильев Олег";
             phone1 = 6532244;
-            phone2 = 6342288;
+            phone2 = 1234567;
+            phone3 = 7654321;
+            phone4 = 9235646;
+            phone5 = 3456789;
+            
             
         } else {
             name1 = args[0];
             name2 = args[1];
             phone1 = Integer.parseInt(args[2]);
             phone2 = Integer.parseInt(args[3]);
+            phone3 = Integer.parseInt(args[4]);
+            phone4 = Integer.parseInt(args[5]);
+            phone5 = Integer.parseInt(args[6]);
         }
         
         PhoneBook myPhoneBook = new PhoneBook();
         
         myPhoneBook.add(name1, phone1);
         myPhoneBook.add(name1, phone2);
-        myPhoneBook.add(name2, phone2);
-        myPhoneBook.add(name2, phone1);
-        myPhoneBook.add(name2, phone2);
+        myPhoneBook.add(name2, phone3);
+        myPhoneBook.add(name2, phone4);
+        myPhoneBook.add(name2, phone5);
         
         System.out.println();
 
